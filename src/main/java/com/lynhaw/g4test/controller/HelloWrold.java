c7feb83330c13d6283fd0c341fb185c9542098c5
hzhuyuanyuan <hzhuyuanyuan@corp.netease.com>
2018-05-29 21:18:55 +0800

    提交日志配置

--- src/main/java/com/lynhaw/g4test/controller/HelloWrold.java
+++ src/main/java/com/lynhaw/g4test/controller/HelloWrold.java
@@ -2,6 +2,7 @@ package com.lynhaw.g4test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.log4j.Logger;
 
 /**
  * @Author yyhu3
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWrold {
     @GetMapping("/helloworld")
-    public String helloWrold()
+    public String helloWrold(String input)
     {
-        return "helloworld";
+        Logger logger = Logger.getLogger(HelloWrold.class);
+        logger.info("输入与输出为:"+input);
+        return input;
     }
 }
